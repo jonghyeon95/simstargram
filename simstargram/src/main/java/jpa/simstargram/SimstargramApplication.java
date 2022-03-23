d4a8ee4161731008aa2314ef7b4fcbd73ea8165f
jonghyeon95 <whdgus5289@naver.com>
2022-03-23 23:47:03 +0900

    -프로젝트 생성 및 gradle 추가

---
+++ simstargram/src/main/java/jpa/simstargram/SimstargramApplication.java
@@ -0,0 +1,13 @@
+package jpa.simstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimstargramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimstargramApplication.class, args);
+	}
+
+}
---
+++ simstargram/src/test/java/jpa/simstargram/SimstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.simstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimstargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
